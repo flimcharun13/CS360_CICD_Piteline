04c6e2ca70ba6237d5c84d215075acc9ae899a8f
flimcharun13 <73243212+flimcharun13@users.noreply.github.com>
2021-12-21 19:59:31 +0700

    Add files via upload

---
+++ Admin.sh
@@ -0,0 +1,9 @@
+sudo yum -y update
+mkdir webservice
+cd webservice
+sudo yum install git
+sudo yum install python3
+pip3 install flask pymongo requests bcrypt gevent WSGIServer qrcode libscrc
+git clone https://github.com/Teejirapat/CS360-hotel-system.git
+cd CS360-hotel-system
+python3 serveradmin.py
