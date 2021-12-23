c937d514dd39687123c1766ba2698be5b9c043a9
flimcharun13 <73243212+flimcharun13@users.noreply.github.com>
2021-12-23 23:00:32 +0700

    Update servermain.py

--- servermain.py
+++ servermain.py
@@ -103,6 +103,7 @@ def profile() :
 						"data" : {
 							"title" : "สำเร็จ",
 							"description" : "ระบบได้ทำการแก้ไขข้อมูลแล้ว",
+							"code" : 200,
 							"icon" : "success",
 							"redirect" : "/"
 						}
