39b0ee91099528ab07cc5b1fd30230eaf7b2de8f
Teejirapat <64017291+Teejirapat@users.noreply.github.com>
2021-12-23 22:38:35 +0700

    Delete Deploy_serveradmin.sh

--- Deploy_serveradmin.sh
+++
@@ -1,7 +0,0 @@
-sudo yum -y update
-sudo yum install git
-sudo yum install python3
-pip3 install flask pymongo requests bcrypt gevent WSGIServer qrcode libscrc
-git clone https://github.com/Teejirapat/CS360-hotel-system.git
-cd CS360-hotel-system
-python3 serveradmin.py
